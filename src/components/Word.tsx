fd1794ff802abf19ab1367cd73f8d8450ce9f83d
Hugo Moran <moranh56@gmail.com>
2023-04-26 16:31:39 -0600

    Add word component

---
+++ src/components/Word.tsx
@@ -0,0 +1,17 @@
+import { type TestLetter } from '../store/types';
+import { Letter } from './Letter';
+
+interface Props {
+	testLetters: TestLetter[];
+}
+
+export const Word = ({ testLetters }: Props) => {
+	return (
+		<div className="flex flex-nowrap">
+			{testLetters.map((testLetter, i) => (
+				<Letter key={i} testLetter={testLetter} />
+			))}
+			<Letter testLetter={{ char: ' ', correct: true }} />
+		</div>
+	);
+};
