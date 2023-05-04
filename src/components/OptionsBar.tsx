4d8ff69b3bb8a4016c5ef595571a50fafd3a99a3
Hugo Moran <moranh56@gmail.com>
2023-05-04 14:43:35 -0600

    Refactor OptionBar component

--- src/components/OptionsBar.tsx
+++ src/components/OptionsBar.tsx
@@ -1,68 +1,113 @@
-import { useState } from 'react';
+import { TestType, TestTypeOption } from '../store/enums';
+import { useTypingTestStore } from '../store/typingTestStore';
+
+interface TestTypeButtonProps {
+	type: TestType;
+	text: string;
+}
+const TestTypeButton = ({ type, text }: TestTypeButtonProps) => {
+	const { testType, setTestType } = useTypingTestStore();
+	return (
+		<button
+			onClick={() => setTestType(type)}
+			className={`btn btn-sm btn-solid-success ${
+				testType === type ? 'underline font-bold' : ''
+			}`}
+		>
+			{text}
+		</button>
+	);
+};
+
+interface TestTypeOptionButtonProps {
+	type: TestTypeOption;
+	text: string;
+}
+const TestTypeOptionButton = ({ type, text }: TestTypeOptionButtonProps) => {
+	const { testTypeOption, setTestTypeOption } = useTypingTestStore();
+	return (
+		<button
+			onClick={() => setTestTypeOption(type)}
+			className={`btn btn-sm btn-solid-warning ${
+				testTypeOption === type ? 'underline font-bold' : ''
+			}`}
+		>
+			{text}
+		</button>
+	);
+};
 
 export const OptionsBar = () => {
-	const [testType, setTestType] = useState(0);
-
-	const testTypeSelectionHandler = (newType: number) => {
-		setTestType(newType);
-	};
+	const { testType } = useTypingTestStore();
 
 	return (
 		<div className="flex flex-wrap justify-center p-3 gap-1 bg-backgroundSecondary rounded-xl">
 			<div className="flex gap-2">
-				<button
-					onClick={() => testTypeSelectionHandler(0)}
-					className={`btn btn-sm btn-solid-success ${
-						testType === 0 ? 'underline font-bold' : ''
-					}`}
-				>
-					Palabras
-				</button>
-				<button
-					onClick={() => testTypeSelectionHandler(1)}
-					className={`btn btn-sm btn-solid-success ${
-						testType === 1 ? 'underline font-bold' : ''
-					}`}
-				>
-					Texto
-				</button>
-				<button
-					onClick={() => testTypeSelectionHandler(2)}
-					className={`btn btn-sm btn-solid-success ${
-						testType === 2 ? 'underline font-bold' : ''
-					}`}
-				>
-					Tiempo
-				</button>
+				<TestTypeButton type={TestType.WORDS} text="Palabras" />
+				<TestTypeButton type={TestType.TEXT} text="Texto" />
+				<TestTypeButton type={TestType.TIME} text="Tiempo" />
 			</div>
 
 			<div className="divider divider-vertical h-8"></div>
 
 			<div
 				id="text-options"
-				className={`gap-2 ${testType === 0 ? 'flex' : 'hidden'}`}
+				className={`gap-2 ${
+					testType === TestType.WORDS ? 'flex' : 'hidden'
+				}`}
 			>
-				<button className="btn btn-sm btn-solid-warning">25p</button>
-				<button className="btn btn-sm btn-solid-warning">50p</button>
-				<button className="btn btn-sm btn-solid-warning">100p</button>
+				<TestTypeOptionButton
+					text="25p"
+					type={TestTypeOption.WORDS_25}
+				/>
+				<TestTypeOptionButton
+					text="50p"
+					type={TestTypeOption.WORDS_50}
+				/>
+				<TestTypeOptionButton
+					text="100p"
+					type={TestTypeOption.WORDS_100}
+				/>
 			</div>
 
 			<div
 				id="words-options"
-				className={`gap-2 ${testType === 1 ? 'flex' : 'hidden'}`}
+				className={`gap-2 ${
+					testType === TestType.TEXT ? 'flex' : 'hidden'
+				}`}
 			>
-				<button className="btn btn-sm btn-solid-warning">Corto</button>
-				<button className="btn btn-sm btn-solid-warning">Medio</button>
-				<button className="btn btn-sm btn-solid-warning">Largo</button>
+				<TestTypeOptionButton
+					text="Corto"
+					type={TestTypeOption.TEXT_SHORT}
+				/>
+				<TestTypeOptionButton
+					text="Medio"
+					type={TestTypeOption.TEXT_MEDIUM}
+				/>
+				<TestTypeOptionButton
+					text="Largo"
+					type={TestTypeOption.TEXT_LONG}
+				/>
 			</div>
 
 			<div
 				id="time-options"
-				className={`gap-2 ${testType === 2 ? 'flex' : 'hidden'}`}
+				className={`gap-2 ${
+					testType === TestType.TIME ? 'flex' : 'hidden'
+				}`}
 			>
-				<button className="btn btn-sm btn-solid-warning">30s</button>
-				<button className="btn btn-sm btn-solid-warning">60s</button>
-				<button className="btn btn-sm btn-solid-warning">120s</button>
+				<TestTypeOptionButton
+					text="30s"
+					type={TestTypeOption.TIME_30}
+				/>
+				<TestTypeOptionButton
+					text="60s"
+					type={TestTypeOption.TIME_60}
+				/>
+				<TestTypeOptionButton
+					text="120s"
+					type={TestTypeOption.TIME_120}
+				/>
 			</div>
 		</div>
 	);
