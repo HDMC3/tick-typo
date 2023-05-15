c92e02d5619d7510d1db834bbf81aba4a7a3600f
Hugo Moran <moranh56@gmail.com>
2023-05-15 15:18:12 -0600

    Add timer and caps lock indicator

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { OptionsBar } from './components/OptionsBar';
 import { useTestTextData } from './hooks/useTestTextData';
 import { useTestLogic } from './hooks/useTestLogic';
 import { useKeyboard } from './hooks/useKeyboard';
+import { TestMode, TypingState } from './store/enums';
 
 function setWordsContainerPosittion() {
 	const activeLetter = document.querySelector<HTMLElement>('.animate-cursor');
@@ -22,11 +23,21 @@ function setWordsContainerPosittion() {
 	}px)`;
 }
 
+function timeToMinutes(time: number) {
+	const minutes = Math.floor(time / 60);
+	return minutes < 10 ? `0${minutes}` : minutes;
+}
+
+function timeToSeconds(time: number) {
+	const seconds = Math.round(time % 60);
+	return seconds < 10 ? `0${seconds}` : seconds;
+}
+
 function App() {
-	const { setTestText } = useTypingTestStore();
+	const { setTestText, typingState, testMode } = useTypingTestStore();
 	const { textData } = useTestTextData();
-	const { time, result } = useTestLogic();
-	const { errors, capsLockOn } = useKeyboard();
+	const { time } = useTestLogic();
+	const { capsLockOn } = useKeyboard();
 
 	useEffect(() => {
 		setTestText(textData.join(' '));
@@ -53,14 +64,24 @@ function App() {
 		<div>
 			<main className="flex flex-col gap-10 py-7 justify-center items-center relative">
 				<OptionsBar />
-				<WordsContainer />
-				<pre>
-					{JSON.stringify(
-						{ time, errors, result, capsLockOn },
-						null,
-						4
-					)}
-				</pre>
+				<div className="flex items-center flex-col gap-8">
+					<WordsContainer />
+					{testMode === TestMode.TIME ? (
+						<div className="w-11/12" style={{ maxWidth: '60rem' }}>
+							<kbd className="kbd text-lg">
+								{timeToMinutes(time)}:{timeToSeconds(time)}
+							</kbd>
+						</div>
+					) : null}
+				</div>
+				{typingState === TypingState.STARTED && capsLockOn ? (
+					<span
+						className={`badge badge-xl px-5 py-5 badge-outline-error gap-4 rounded-md`}
+					>
+						<span className={`dot dot-error`}></span>
+						Caps Lock
+					</span>
+				) : null}
 			</main>
 		</div>
 	);
