d08b7a5c6a8fe0db063eb6754f6cdc53918cf83b
Hugo Moran <moranh56@gmail.com>
2023-05-16 22:41:16 -0600

    Fix effect

--- src/hooks/useKeyboard.ts
+++ src/hooks/useKeyboard.ts
@@ -49,7 +49,7 @@ export const useKeyboard = () => {
 		return () => {
 			removeEventListener('keyup', handleKeyUp);
 		};
-	});
+	}, []);
 
     useEffect(() => {
         if (typingState === TypingState.PENDING) {
